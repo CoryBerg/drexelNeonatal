1d1bb88b75034a671887ecc4f6a863bcde70c216
donxu29 <tx29@n1-141-198.dhcp.drexel.edu>
2014-04-29 01:25:59 -0400

    Popup Message & Disabled Pixel Perfect

---
+++ Assets/Scripts/TextPopup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextPopup : MonoBehaviour {
+
+	private GameObject popup;
+	private string action;
+
+	public void OnClick(dfControl control, dfMouseEventArgs mouseEvent) {
+		Debug.Log (this.transform.name);
+
+		action = "Current Operation: " + this.GetComponent<dfButton>().Text;
+		StartCoroutine(ShowMessage(action, 3));
+
+
+
+	}
+
+	IEnumerator ShowMessage (string message, float delay) {
+
+		popup = GameObject.Find ("PopupMessage");
+
+		popup.GetComponent<dfLabel>().Text = message;
+		popup.GetComponent<dfLabel>().IsVisible = true;
+		yield return new WaitForSeconds(delay);
+		popup.GetComponent<dfLabel>().IsVisible = false;
+	}
+}
