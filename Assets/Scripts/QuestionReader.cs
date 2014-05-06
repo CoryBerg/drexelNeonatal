af631bdf20607cf2807d0eea5660313d0e606fa1
donxu29 <tx29@n1-141-190.dhcp.drexel.edu>
2014-05-05 22:57:11 -0400

    question script -- in progress

---
+++ Assets/Scripts/QuestionReader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestionReader : MonoBehaviour {
+
+	public GameObject questionGUI;
+	public GameObject aAGUI;
+	public GameObject aBGUI;
+	public GameObject aCGUI;
+	public GameObject aDGUI;
+
+
+	public class Question{
+		public string questionText;
+		public string anwerAText;
+
+		public Question(string qText, string aAText){
+			questionText = qText;
+			anwerAText = aAText;
+		}
+	}
+
+	public Question[] arrayOfQuestions;
+
+	void Start(){
+		arrayOfQuestions = new Question[10];
+
+		for (int i = 0; i < 10; i++) {
+
+			string someQ = "Q number: " + i;
+			string someA = "A number: " + i;
+
+			Question freshQ = new Question(someQ, someA);
+			arrayOfQuestions[i] = freshQ;
+		}
+	}
+}
