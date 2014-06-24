079a83cbe5f27e1049d170b22a33f5f00d916fbf
Timothy Day <the.tday@gmail.com>
2014-06-23 21:12:01 -0400

    Test notification and waiting implemented.

---
+++ Assets/Scripts/DoTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoTest : MonoBehaviour {
+	public string TestName;
+	public float TestLength;
+	public dfPanel resultPanel;
+	public dfPanel testPanel;
+
+	public void OnClick(dfControl control, dfMouseEventArgs mouseEvent) {
+		if(TestHandler.Instance.TestStatus(TestName)) {
+			resultPanel.IsVisible = true;
+			testPanel.IsVisible = false;
+		} else if(!TestHandler.Instance.TestInProgressButNotComplete(TestName)) {
+			TestHandler.Instance.BeginTest(TestName,TestLength);
+		} else {
+			TestHandler.Instance.StartCoroutine("ResultsSoon");
+			// Should do something if it's already in progress
+		}
+	}
+}
--- Assets/Scripts/TestHandler.cs
+++ Assets/Scripts/TestHandler.cs
@@ -1,14 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestHandler : MonoBehaviour {
 	// Use this for initialization
-	void Start () {
-	
+	public dfLabel lbl;
+	public Dictionary<string,bool> TestsCompleted; // if dict has key, it's in progress, if not--not started.
+	public static TestHandler Instance;
+	public dfTweenFloat tweener, reverseTweener;
+	// If true... results are available
+
+	public class TestStuff {
+		public string name;
+		public float length;
+		public TestStuff (string n, float l) {
+			this.name = n;
+			this.length = l;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void Start () {
+		TestsCompleted = new Dictionary<string, bool>();
+		Instance = this;
+	}
+
+	public void BeginTest(string testName, float testLength) {
+		StartCoroutine("DoTest",new TestStuff(testName,testLength));
+	}
+
+	public IEnumerator ResultsSoon() {
+		
+		lbl.Text = "Results Available Soon";
+		// This tweening will be buggy.
+		if(!tweener.IsPlaying) {
+			tweener.Play();
+		}
+		yield return new WaitForSeconds(4.5f);
+		if(!reverseTweener.IsPlaying) {
+			reverseTweener.Play();
+		}
+	}
+
+	IEnumerator DoTest(TestStuff aTest) {
+		TestsCompleted[aTest.name] = false;
+		yield return new WaitForSeconds(aTest.length);
+		TestsCompleted[aTest.name] = true;
+		lbl.Text = aTest.name + " Results Available";
+		// This tweening will be buggy.
+		if(!tweener.IsPlaying) {
+			tweener.Play();
+		}
+		yield return new WaitForSeconds(4.5f);
+		if(!reverseTweener.IsPlaying) {
+			reverseTweener.Play();
+		}
+	}
+
+
+
+	public bool TestStatus(string testName) {
+		if(!TestsCompleted.ContainsKey(testName)) {
+			return false;
+		}
+		return TestsCompleted[testName];
+	}
+
+	public bool TestInProgressButNotComplete(string testName) {
+		return TestsCompleted.ContainsKey(testName) && !TestsCompleted[testName];
 	}
 }
