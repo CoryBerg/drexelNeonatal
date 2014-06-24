b0d622c0ef6f1b6232fddf4262dad2502841d0e6
Timothy Day <the.tday@gmail.com>
2014-06-24 16:17:21 -0400

    Fixed null ref on intervention... but animations don't play

--- Assets/Scripts/MakeChoice.cs
+++ Assets/Scripts/MakeChoice.cs
@@ -9,7 +9,7 @@ public class MakeChoice : MonoBehaviour {
 	public void OnClick(dfControl control, dfMouseEventArgs mouseEvent) {
 		baby = GameObject.FindGameObjectWithTag ("Baby");
 		Debug.Log (this.transform.name);
-
+		print (baby.name);
 		baby.GetComponent<BabyAnimatorController>().currentState = transform.name;
 		baby.GetComponent<BabyAnimatorController>().hasChanged = true;
 		CaseInitializer.Instance.ActiveCase.isCorrect = IsAnswer;
