ead603aea1dc2405223f60c7bf353593e0f0f189
CoryBerg <cory3bergquist@gmail.com>
2014-06-25 22:53:58 -0400

    Added new animator handler framework, organized scripts

---
+++ Assets/Scripts/Animations/ArmAnimationContainer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArmAnimationContainer : MonoBehaviour {
+	private List<string> animations;
+
+	// Use this for initialization
+	void Awake() {
+		animations = new List<string> ()
+			{
+				"Idle",
+				"NeedleDecomp",
+				"UseStethoscope",
+				"ChestCompression",
+				"Sunction",
+				"Intubation",
+			};
+	}
+
+	public string GetAnimation(int index) {
+		return animations [index];
+	}
+}
---
+++ Assets/Scripts/Animations/ArmAnimatorHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmAnimatorHandle : MonoBehaviour {
+	public Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+
+	// Triggers animation by animation name
+	private void TriggerAnimation(string animation) {
+		animator.SetTrigger (animation);
+	}
+}
