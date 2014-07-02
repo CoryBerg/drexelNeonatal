93d4c6c3f1cf3fb3fd4d4ab65f89251c47c94014
Tim Day <the.tday@gmail.com>
2014-07-02 02:30:42 -0400

    Added a main menu

---
+++ Assets/Scripts/UI/SetScenarioAndLoad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Attach to a daikon button and load selected scenario
+public class SetScenarioAndLoad : MonoBehaviour {
+	public NeonatalCase CaseType;
+
+	public void OnClick( dfControl control, dfMouseEventArgs mouseEvent ) {
+		CaseHandler.Instance.ActivateCase(CaseType);
+		Application.LoadLevel("IntroRespCase");
+	}
+}
