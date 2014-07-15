53a5cbcd3ff7d012dba86a57f5fb93cd96f11bc4
Tim Day <the.tday@gmail.com>
2014-07-15 00:02:28 -0400

    Buttons for chest compression in.

---
+++ Assets/Scripts/ChestCompressionController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestCompressionController : MonoBehaviour {
+	public dfButton[] mainButtons;
+	public ArmAnimatorController animController;
+
+	public void HideMainButtons() {
+		ButtonChange(false);
+	}
+
+	void ButtonChange(bool on) {
+		foreach(dfButton b in mainButtons) {
+			b.IsVisible = on;
+		}
+	}
+
+	public void BeginCompression() {
+		// starts compression or continues if it's already going
+	}
+
+	public void StopCompression() {
+		// ends compression, closes controls
+		ButtonChange(true);
+	}
+}
